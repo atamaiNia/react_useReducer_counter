4b545730060781b56aa01404e5d35d9a6b8cabbf
atamaiNia <bimatideka@gmail.com>
2022-11-15 13:44:33 +0200

    completed

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,31 +1,39 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import './Counter.css';
 
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return state + action.payload;
+
+    case 'decrement':
+      return state - action.payload;
+
+    default:
+      throw new Error(`Unsupported action type ${action.type}`);
+  }
+}
+
 export default function Counter() {
-  const [count, setCount] = useState(100);
-
-  const handleIncrement = () => {
-    setCount(prevState => prevState + 1);
-  };
-
-  const handleDecrement = () => {
-    setCount(prevState => prevState - 1);
-  };
+  const [count, dispatch] = useReducer(countReducer, 100);
 
   return (
     <div className="Counter">
       <span className="Counter__value">{count}</span>
       <div className="Counter__controls">
-        <button type="button" onClick={handleIncrement}>
+        <button
+          type="button"
+          onClick={() => dispatch({ type: 'increment', payload: 1 })}
+        >
           Збільшити на 1
         </button>
-        <button type="button" onClick={handleDecrement}>
+        <button
+          type="button"
+          onClick={() => dispatch({ type: 'decrement', payload: 1 })}
+        >
           Зменшити на 1
         </button>
       </div>
-      <button type="button" onClick={() => setCount(100)}>
-        Reset
-      </button>
     </div>
   );
 }
